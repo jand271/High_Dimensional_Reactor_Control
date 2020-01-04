8945701e791224aa33e0d0da4f827f7b438cef67
Jason Anderson <jand271@stanford.edu>
2020-01-03 22:05:08 -0800

    Added Q and R capability

--- control/heat_exchanger_mpc_controller.py
+++ control/heat_exchanger_mpc_controller.py
@@ -4,7 +4,7 @@ from control.cftoc_solver import CFTOCSolver
 
 
 class HeatExchangerMPCController(object):
-    def __init__(self, fem_model, initial_temperature, horizon=10, max_removal_power_density=1):
+    def __init__(self, fem_model, initial_temperature, horizon=10, max_removal_power_density=1, Q=None, R=None):
         """ Check Constructor Inputs """
         assert isinstance(fem_model, FEMModel)
         assert isinstance(initial_temperature, int) or isinstance(initial_temperature, float)
@@ -15,7 +15,7 @@ class HeatExchangerMPCController(object):
 
         T0 = initial_temperature * np.ones((A.shape[0],))
 
-        self.solver = CFTOCSolver(A, B, f, T0, T0, horizon, umax=max_removal_power_density)
+        self.solver = CFTOCSolver(A, B, f, T0, T0, horizon, umax=max_removal_power_density, Q=Q, R=R)
 
     def set_desired_state(self, state):
         """ set xbar of solver """
