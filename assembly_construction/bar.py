cb53d0d73501410835fbe024fe2d30e1fafa5dfd
Jason Anderson <jand271@stanford.edu>
2020-01-05 16:56:02 -0800

    added rectangular bar component

---
+++ assembly_construction/bar.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+from assembly_construction.component import Component
+
+
+class Bar(Component):
+    def __init__(self, x_center, y_center, x_length, y_length, plot_color='k', material=None):
+        """
+        Bar Constructor
+        :param x_center: x position of Component
+        :param y_center: y position of Component
+        :param x_length: length of bar in x direction
+        :param y_length: length of bar in y direction
+        :param plot_color: applicable plotting color
+        :param material: material of component
+        """
+        super().__init__(x_center, y_center, plot_color, material=material)
+
+        """ Check Constructor Inputs """
+        assert isinstance(x_length, float) or isinstance(x_length, int)
+        assert isinstance(y_length, float) or isinstance(y_length, int)
+
+        self._x_length = x_length
+        self._y_length = y_length
+
+    def is_point_within(self, x, y):
+        """ Checks if input position (x,y) is within the rod """
+        return abs(x - self._x_position) <= self._x_length / 2 and abs(y - self._y_position) <= self._y_length / 2
+
+    def plot(self):
+        plt.gca().add_patch(
+            plt.Rectangle((self._x_position - self._x_length / 2, self._y_position - self._y_length / 2),
+                          self._x_length, self._y_length, color=self._plot_color,
+                          alpha=0.5))
+
+
+class SquareBar(Bar):
+    def __init__(self, x_center, y_center, side_length, plot_color='k', material=None):
+        """
+        Square Bar Constructor
+        :param x_center: x position of Component
+        :param y_center: y position of Component
+        :param side_length: side length of bar
+        :param plot_color: applicable plotting color
+        :param material: material of component
+        """
+        super().__init__(x_center, y_center, side_length, side_length, plot_color=plot_color, material=material)
---
+++ assembly_construction/test/test_bar.py
@@ -0,0 +1,77 @@
+import unittest
+from assembly_construction.material import Material
+from assembly_construction.bar import Bar, SquareBar
+
+
+class TestSquareBar(unittest.TestCase):
+    def test_constructor(self):
+        b1 = SquareBar(0, 0, 1)
+        assert (0, 0) == b1.get_position()
+
+        b2 = SquareBar(0, 0, 1, plot_color='k')
+        assert 'k' == b2.get_plot_color()
+
+        material = Material()
+        b3 = SquareBar(0, 0, 1, plot_color='k', material=material)
+        assert material == b3.get_material()
+
+    def test_is_point_within(self):
+        r = SquareBar(0, 0, 1)
+
+        assert r.is_point_within(0, 0)
+        assert r.is_point_within(0.5, 0.5)
+        assert r.is_point_within(0.5, -0.5)
+        assert r.is_point_within(-0.5, 0.5)
+        assert r.is_point_within(-0.5, -0.5)
+        assert r.is_point_within(0, 0.25)
+        assert r.is_point_within(0.25, 0)
+        assert r.is_point_within(0, -0.25)
+        assert r.is_point_within(-0.25, 0)
+
+        assert not r.is_point_within(0, 2)
+        assert not r.is_point_within(2, 0)
+        assert not r.is_point_within(0, -2)
+        assert not r.is_point_within(-2, 0)
+        assert not r.is_point_within(1.5, 1.5)
+        assert not r.is_point_within(1.5, -1.5)
+        assert not r.is_point_within(-1.5, 1.5)
+        assert not r.is_point_within(-1.5, -1.5)
+
+
+class TestBar(unittest.TestCase):
+    def test_constructor(self):
+        b1 = Bar(0, 0, 1, 2)
+        assert (0, 0) == b1.get_position()
+
+        b2 = Bar(0, 0, 1, 2, plot_color='k')
+        assert 'k' == b2.get_plot_color()
+
+        material = Material()
+        b3 = Bar(0, 0, 1, 2, plot_color='k', material=material)
+        assert material == b3.get_material()
+
+    def test_is_point_within(self):
+        r = Bar(0, 0, 1, 2)
+
+        assert r.is_point_within(0, 0)
+        assert r.is_point_within(0.5, 1.0)
+        assert r.is_point_within(0.5, -1.0)
+        assert r.is_point_within(-0.5, 1.0)
+        assert r.is_point_within(-0.5, -1.0)
+        assert r.is_point_within(0, 0.5)
+        assert r.is_point_within(0.25, 0)
+        assert r.is_point_within(0, -0.5)
+        assert r.is_point_within(-0.25, 0)
+
+        assert not r.is_point_within(0, 4)
+        assert not r.is_point_within(2, 0)
+        assert not r.is_point_within(0, -4)
+        assert not r.is_point_within(-2, 0)
+        assert not r.is_point_within(1.5, 3.0)
+        assert not r.is_point_within(1.5, -3)
+        assert not r.is_point_within(-1.5, 3.0)
+        assert not r.is_point_within(-1.5, -3)
+
+
+if __name__ == '__main__':
+    unittest.main()
