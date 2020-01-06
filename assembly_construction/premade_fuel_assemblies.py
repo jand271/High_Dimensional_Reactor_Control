5d91ef622772e5d508e9d4776f6382c784f2e4fc
Jason Anderson <jand271@stanford.edu>
2020-01-05 16:56:36 -0800

    created two easy premade assemblies

---
+++ assembly_construction/premade_fuel_assemblies.py
@@ -0,0 +1,113 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from assembly_construction.component_assembly import ComponentAssembly
+from assembly_construction.component import UnshapedComponent
+from assembly_construction.rod import Rod
+from assembly_construction.bar import SquareBar
+from assembly_construction.material import Material
+
+
+class ComponentAssemblyA(ComponentAssembly):
+
+    def __init__(self):
+        """ creates heat exchanger with 17 cylinder rods """
+        material = Material(thermal_conductivity=1., specific_heat_capacity=1., density=1.)
+        super().__init__(default_component=UnshapedComponent(material=material, plot_color='k'))
+
+        nh = 8
+        nc = 9
+        r = 0.1
+        heat_power_density = 1000
+
+        # cooling rods
+        cooling_rods = [Rod(0, 0, r, plot_color='b', material=material)]
+        R = 0.6
+        for t in np.linspace(0, 2 * np.pi, nc):
+            if t == 0:
+                continue
+            cooling_rods.append(Rod(R * np.cos(t), R * np.sin(t), r, plot_color='b', material=material))
+
+        # heating rods
+        heating_rods = []
+        R = 0.33
+        i = 0
+        for t in np.linspace(0, 2 * np.pi, nh + 1):
+            i += 1
+            if t == 0:
+                continue
+            if i % 2 == 0:
+                heating_rods.append(Rod(R * np.cos(t), R * np.sin(t), r, plot_color='r', material=material))
+            else:
+                cooling_rods.append(Rod(R * np.cos(t), R * np.sin(t), r, plot_color='b', material=material))
+                nh += -1
+                nc += 1
+
+        cool_power_density = -heat_power_density * nh / nc
+
+        for rod in heating_rods:
+            rod.set_volumetric_power_density(heat_power_density)
+            self.add_component(rod, component_set='set_q_dot')
+
+        for rod in cooling_rods:
+            rod.set_volumetric_power_density(cool_power_density)
+            self.add_component(rod, component_set='controllable_q_dot')
+
+
+class ComponentAssemblyB(ComponentAssembly):
+
+    def __init__(self):
+        """ creates heat exchanger with 17 cylinder bars """
+        material = Material(thermal_conductivity=1., specific_heat_capacity=1., density=1.)
+        super().__init__(default_component=UnshapedComponent(material=material, plot_color='k'))
+
+        nh = 8
+        nc = 9
+        r = 0.15
+        heat_power_density = 1000
+
+        # cooling bars
+        cooling_bars = [SquareBar(0, 0, r, plot_color='b', material=material)]
+        R = 0.6
+        for t in np.linspace(0, 2 * np.pi, nc):
+            if t == 0:
+                continue
+            cooling_bars.append(SquareBar(R * np.cos(t), R * np.sin(t), r, plot_color='b', material=material))
+
+        # heating bars
+        heating_bars = []
+        R = 0.33
+        i = 0
+        for t in np.linspace(0, 2 * np.pi, nh + 1):
+            i += 1
+            if t == 0:
+                continue
+            if i % 2 == 0:
+                heating_bars.append(SquareBar(R * np.cos(t), R * np.sin(t), r, plot_color='r', material=material))
+            else:
+                cooling_bars.append(SquareBar(R * np.cos(t), R * np.sin(t), r, plot_color='b', material=material))
+                nh += -1
+                nc += 1
+
+        cool_power_density = -heat_power_density * nh / nc
+
+        for bar in heating_bars:
+            bar.set_volumetric_power_density(heat_power_density)
+            self.add_component(bar, component_set='set_q_dot')
+
+        for bar in cooling_bars:
+            bar.set_volumetric_power_density(cool_power_density)
+            self.add_component(bar, component_set='controllable_q_dot')
+
+
+if __name__ == '__main__':
+    fa_A = ComponentAssemblyA()
+    fa_A.plot()
+    plt.savefig('Fuel_Assembly_A.png')
+
+    plt.clf()
+
+    fa_B = ComponentAssemblyB()
+    fa_B.plot()
+    plt.savefig('Fuel_Assembly_B.png')
+
+    plt.clf()
