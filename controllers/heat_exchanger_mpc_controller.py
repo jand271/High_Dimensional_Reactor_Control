5b01581a36fd18fedc0fd09296c1102b0a39ad35
Jason Anderson <jand271@stanford.edu>
2020-02-04 19:20:12 -0800

    removed branch contamination of master

--- controllers/heat_exchanger_mpc_controller.py
+++ controllers/heat_exchanger_mpc_controller.py
@@ -1,7 +1,7 @@
 import numpy as np
 from modeling.fem_model import FEMModel
 from controllers.solvers import CFTOCSolver
-from controllers.model_reduction import proper_orthogonal_decomposition_model_reduction
+
 
 class HeatExchangerMPCController(object):
     def __init__(self, fem_model, initial_temperature, horizon=10, max_removal_power_density=1, Q=None, R=None):
@@ -13,10 +13,6 @@ class HeatExchangerMPCController(object):
 
         A, B, f = fem_model.state_transition_model()
         T0 = initial_temperature * np.ones((A.shape[0],))
-
-        phi, A, B, f, Q = proper_orthogonal_decomposition_model_reduction(A, B, f, 25, Q=Q)
-        T0 = phi.T @ T0
-        self.phi = phi
 
         self.solver = CFTOCSolver(A, B, f, T0, T0, horizon, umax=max_removal_power_density, Q=Q, R=R)
 
@@ -30,4 +26,4 @@ class HeatExchangerMPCController(object):
         :param current_position: current position
         :return: optimal velocity command
         """
-        return self.solver.calculate_optimal_actuation(self.phi.T @ current_position)
+        return self.solver.calculate_optimal_actuation(current_position)
