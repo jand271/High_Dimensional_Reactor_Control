78ccefb146a02b91bbaa1197dd1e38c556a03a3b
Jason Anderson <jand271@stanford.edu>
2020-02-27 17:36:17 -0800

    added carlberg model reduction method

---
+++ model_reduction/carlberg.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.linalg import eigh
+from model_reduction.weighted_pod_model_reduction import WeightedPODModelReduction
+
+
+class Carlberg(WeightedPODModelReduction):
+
+    def compute_reduction_basis(self):
+        self.V = self.algorithm_1(self.r, self.X, self.C_full.T @ self.C_full)
+
+    @staticmethod
+    def algorithm_1(rank, X, Theta):
+        Theta_bar = X.T @ Theta @ X
+        (r, c) = Theta_bar.shape
+        Sigma_squared, V = eigh(Theta_bar, eigvals=(c - rank, c - 1))
+        SinvV = np.flip(np.divide(V, np.sqrt(Sigma_squared)), 1)
+        return X @ SinvV
+
+    @staticmethod
+    def algorithm_2(rank, X, square_root_Theta):
+        X_bar = square_root_Theta @ X
+        U, S, ZT = PODModelReduction.compute_truncated_svd(X_bar, rank)
+        SinvZ = np.divide(ZT.T, S)
+        return X @ SinvZ
