14f72915b2da13a9dfe8659ab525ca23bae50a7f
Jason Anderson <jand271@stanford.edu>
2020-02-28 15:28:36 -0800

    added easy import for model reduction

---
+++ model_reduction/__init__.py
@@ -0,0 +1,7 @@
+from .balanced_truncation import BalancedTruncation
+from .buithanh import BuiThanh
+from .carlberg import Carlberg
+from .gradient_descent_weighted_pod_model_reduction import GradientDescentWeightedPODModelReduction
+from .md_model_reduction import MDModelReduction
+from .model_reduction import ModelReduction, GalerkinModelReduction
+from .pod_model_reduction import PODModelReduction
