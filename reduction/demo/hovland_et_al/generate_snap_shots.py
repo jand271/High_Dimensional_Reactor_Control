ee850bb541fb43d9278363dd6353b3d3257fe2d7
Jason Anderson <jand271@stanford.edu>
2020-03-13 16:41:17 -0700

    added control inputs to snap shots

--- reduction/demo/hovland_et_al/generate_snap_shots.py
+++ reduction/demo/hovland_et_al/generate_snap_shots.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
     X = np.zeros((nx, ns))
     X[:, 0] = np.array([-0.9044, -9.1380, -2.5036, 0.6696, -0.0821, -4.0350])
 
-    u = np.array([1])
+    U = np.ones((nu, ns - 1))
 
     for i in range(1, ns):
-        X[:, i] = A @ X[:, i - 1] + B @ u
+        X[:, i] = A @ X[:, i - 1] + B @ U[:, i - 1]
 
-    savemat('snapshots_of_hovland_el_al.mat', {'X': X})
+    savemat('snapshots_of_hovland_el_al.mat', {'X': X, 'U': U})
