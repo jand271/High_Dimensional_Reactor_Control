218db7ead7ee8a1267b975d5adb8ab9d91c3fa1d
Jason Anderson <jand271@stanford.edu>
2020-03-15 16:19:44 -0700

    updated to include control inputs

--- reduction/demo/heat_exchanger/results.py
+++ reduction/demo/heat_exchanger/results.py
@@ -1,13 +1,17 @@
 from scipy.io import loadmat
 
-from reduction.demo.utils import plot_orthogonal_error
+from reduction.demo.utils import plot_orthogonal_error, plot_reduction_error
 
 if __name__ == "__main__":
     model = loadmat('heat_exchanger_model.mat')
+    A = model["A"]
+    B = model["B"]
     C = model["C"]
+    f = model["f"].flatten()
 
     snapshots = loadmat('snapshots_heat_exchanger_model.mat')
     X = snapshots["X"]
+    U = snapshots["U"]
 
     reduction_list = [
         'buithanh',
@@ -20,3 +24,4 @@ if __name__ == "__main__":
     max_rank_buithanh = 0
 
     plot_orthogonal_error(X, C, reduction_list, rank_list, max_rank_buithanh=max_rank_buithanh)
+    plot_reduction_error(X, U, A, B, C, reduction_list, rank_list, max_rank_buithanh=max_rank_buithanh, f=f)
