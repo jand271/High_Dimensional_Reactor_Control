8dcbb1d17c51a590b3fc65ad13a58248e86fe5b1
Jason Anderson <jand271@stanford.edu>
2021-04-12 10:34:35 -0700

    Added coverage

--- .github/workflows/execute_python_unittests_to_xml.py
+++ .github/workflows/execute_python_unittests_to_xml.py
@@ -1,10 +1,19 @@
-import unittest
 import sys
-import xmlrunner
+from unittest import TestLoader
+from xmlrunner import XMLTestRunner
+from coverage import Coverage
 
 if __name__ == "__main__":
 
     path_to_execute_tests = sys.argv[1]
-    path_to_save_xml_file = sys.argv[2]
-    test_suite = unittest.TestLoader().discover(path_to_execute_tests)
-    rest_results = xmlrunner.XMLTestRunner(verbosity=2, output=path_to_save_xml_file).run(test_suite)
+    path_to_save_test_xml_file = sys.argv[2]
+    path_so_save_coverage_xml_file = sys.argv[3]
+
+    cov = Coverage()
+    cov.start()
+
+    test_suite = TestLoader().discover(path_to_execute_tests)
+    test_results = XMLTestRunner(verbosity=2, output=path_to_save_test_xml_file).run(test_suite)
+
+    cov.stop()
+    cov.xml_report(outfile=path_so_save_coverage_xml_file)
